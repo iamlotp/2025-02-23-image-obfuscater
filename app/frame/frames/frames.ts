2a114cd9ac0d11b9f56d0e47f21b67b0da852f1d
REZA <roojeld@gmail.com>
2025-02-26 03:21:13 +0330

    added airstack verification

--- app/frame/frames/frames.ts
+++ app/frame/frames/frames.ts
@@ -1,7 +1,25 @@
 import { createFrames } from "frames.js/next";
+import { farcasterHubContext } from "frames.js/middleware";
+
 import { APP_URL } from "../constants";
 
 export const frames = createFrames({
     basePath: "/frame/frames",
     baseUrl: APP_URL,
+    middleware: [
+        farcasterHubContext({
+          ...(process.env.NODE_ENV === "production"
+            ? {
+              hubHttpUrl: "https://hubs.airstack.xyz",
+              hubRequestOptions: {
+                headers: {
+                  "x-airstack-hubs": process.env.AIRSTACK_API_KEY as string,
+                },
+              },
+            }
+            : {
+              hubHttpUrl: "http://localhost:3010/hub",
+            }),
+        }),
+      ]
 });
